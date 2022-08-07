70459976c918715fc369433f76fd8a31ef49f16c
Roman Raspopov <=>
2022-08-07 13:02:40 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+
+   <section id="catalog" class="catalog">
+
+      <ProductsList :products="products" />
+
+      <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
+
+  </section>
+
+</template>
+
+<script>
+
+import products from '@/data/products';
+import ProductsList from '@/components/ProductList.vue';
+import BasePagination from '@/components/BasePagination.vue';
+
+export default {
+  name: 'App',
+ 
+  components: { ProductsList , BasePagination },
+   data() {
+    return {
+      page: 1,
+      productsPerPage: 3,
+      products,
+    };
+  },
+  computed: {
+    products() {
+      const offsest = (this.page - 1) * this.productsPerPage;
+      return products.slice(offsest, offsest + this.productsPerPage);
+    },
+    countProducts() {
+       return products.length;
+    }
+  }
+
+};
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="catalog__list">
+  <ProductsItem :product="product" v-for="(product, index) in products" :key="index"/>
+  </ul>
+</template>
+
+<script>
+import ProductsItem from './ProductItem.vue';
+
+export default {
+  props: ['products'],
+  components: { ProductsItem },
+ 
+};
+</script>
